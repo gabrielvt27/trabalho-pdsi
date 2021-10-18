8c280d47645603ba0724131ffe91719c45830cee
Gabrielvt <35491313+gabrielvt27@users.noreply.github.com>
2021-10-18 19:21:37 -0300

    Add files via upload

--- backend/laissez/src/main/java/com/pdsi/laissez/repositories/UsuarioRepository.java
+++ backend/laissez/src/main/java/com/pdsi/laissez/repositories/UsuarioRepository.java
@@ -1,9 +1,15 @@
 package com.pdsi.laissez.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.pdsi.laissez.models.Produto;
 import com.pdsi.laissez.models.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+	@Query(value="select * from usuario where endereco_email = ?1 and senha = ?2", nativeQuery = true)
+	List<Usuario> findByEmailouSenha(String email, String senha);
 }
